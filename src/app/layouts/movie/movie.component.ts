747bcbc3aa4f07e867b57a21260fb4a74cafc623
Louis Sutton <louis.sutton.iii@gmail.com>
2020-01-14 03:51:47 -0800

    Added Logic and Fixed Background

--- src/app/layouts/movie/movie.component.ts
+++ src/app/layouts/movie/movie.component.ts
@@ -1,13 +1,17 @@
+import { MoviesService } from 'src/app/core/services/movies.service';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
   styleUrls: ['./movie.component.scss']
 })
 export class MovieComponent implements OnInit {
-
-  constructor() { }
+  movieData;
+  constructor(service: MoviesService) { 
+    this.movieData = service.getMovieData
+    ('star-wars-the-rise-of-skywalker')[0];
+    console.log(this.movieData);
+  }
 
   ngOnInit() {
   }
